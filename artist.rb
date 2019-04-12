3cc06f05fb3a39f56217a6c2b5f2406ff1251402
Shawn Ren <shawnren527@gmail.com>
2019-04-12 18:56:24 +0000

    Automatically backed up by Learn

--- artist.rb
+++ artist.rb
@@ -1,7 +1,7 @@
 class Artist < ActiveRecord::Base
   attr_accessor :name, :age, :genre, :hometown
 
-  def initialize(name: name, age: age, genre: genre, hometown: hometown)
+  def initialize(name: name = nil, age: age = nil, genre: genre = nil, hometown: hometown = nil)
     @name = name
     @age = age
     @genre = genre
