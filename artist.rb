ac5b5a909fe457994160f99993c20cc9b0be699f
Shawn Ren <shawnren527@gmail.com>
2019-04-12 18:18:17 +0000

    Automatically backed up by Learn

--- artist.rb
+++ artist.rb
@@ -1,3 +1,9 @@
 class Artist
   attr_accessor :name, :age, :genre, :hometown
+
+  initialize(name: name, age: age, genre: genre, hometown: hometown)
+  @name = name
+  @age = age
+  @genre = genre
+  @hometown = hometown
 end
