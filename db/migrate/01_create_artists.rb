4f2c1da1304b860924e0f36644b854cb0e0d57ee
Shawn Ren <shawnren527@gmail.com>
2019-04-12 18:48:58 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,6 +1,6 @@
 class CreateArtists < ActiveRecord::Migration[5.2]
   def change
-    creat_table :artists do |t|
+    create_table :artists do |t|
       t.string :name
       t.string :genre
       t.integer :age
