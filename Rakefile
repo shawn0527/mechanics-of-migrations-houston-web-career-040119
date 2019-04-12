aa624c9b910a5ea61c77262a407ee9fbb4f050d5
Shawn Ren <shawnren527@gmail.com>
2019-04-12 18:10:52 +0000

    Automatically backed up by Learn

--- Rakefile
+++ Rakefile
@@ -1,6 +1,7 @@
 require_relative './config/environment'
 require 'sinatra/activerecord/rake'
 
+
 task :console do
   require 'irb'
   ARGV.clear
---
+++ db/migrate/01_create_artists.rb
@@ -0,0 +1,10 @@
+class CreateArtists < ActiveRecord::Migration
+  def change
+    creat_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
+  end
+end
